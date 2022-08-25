9020e8e1fd663e1072981e777c845d3c91ba3366
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-08-24 20:11:36 -0700

    return object key based on corresponding value input

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+// assetEqual function for test code
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKeyByValue function - should receive an object and a value, and return
+// the first key which contains the give value. If no such key exists, return
+// undefined.
+
+findKeyByValue = function (object, value) {
+  for (const key in object) {
+    const objectValue = object[key];
+    if (objectValue === value) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
