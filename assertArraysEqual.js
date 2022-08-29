138c8d269504e12bb47f399f1a61d5006445b799
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-08-29 13:12:29 -0700

    refactored assertArraysEqual so that it requires needed function and exports itself for testing in separate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,9 @@
+// Require eqArrays function
+
+const eqArrays = require('./eqArrays');
+
+// assertArraysEqual function
+
 const assertArraysEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
@@ -6,20 +12,6 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
   }
 };
 
-function eqArrays(array1, array2) {
-  if (!array1 || !array2) {
-    return false;
-  } else if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
+// export assertArraysEqual so it can be retrieved for testing
 
-let array10 = [1, 2, 3];
-let array20 = [1, 2, 3];
-console.log(assertArraysEqual(array10, array20))
+module.exports = assertArraysEqual;
